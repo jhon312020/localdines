9c18f5a9698010219100248567cd98553d5a44eb
sivakrishnan <siva@proisc.com>
2023-03-10 16:22:52 +0530

    update the extra content view add class in admin.css for extra view

--- app/views/pjAdminPosOrders/pjActionGetExtras.php
+++ app/views/pjAdminPosOrders/pjActionGetExtras.php
@@ -28,14 +28,14 @@ if ($tpl['extra_val'] == 1) {
   </thead>
   <tbody id="fdExtraTable_show_<?php echo $index; ?>">
     <?php for ($i=1; $i <= $tpl['qty'] ; $i++) { ?>
-      <tr class="page-link <?php echo $i == 1 ? "table-bg-primary" : "table-bg-default"; ?>" data-page_container="#fdExtraTable_show_<?php echo $index; ?>" data-qty_container="#extra_qty" data-page="<?php echo "qty_".$i; ?>">
+      <tr <?php if(!$tpl['edit']) { ?> class="page-link <?php echo $i == 1 ? "table-bg-primary" : "table-bg-default"; ?>" <?php } ?> data-page_container="#fdExtraTable_show_<?php echo $index; ?>" data-qty_container="#extra_qty" data-page="<?php echo "qty_".$i; ?>">
         <td scope="row" class="index"><?php echo $i; ?></td>
         <td id="load_data_<?php echo $index; ?>_<?php echo "qty_".$i; ?>" class="load_data row">
           <?php foreach($selected_arr as $selected_value) { ?>
             <?php if ($selected_value['qty_no'] == "qty_".$i) { ?>
-            <div class="form-group col-sm-4">
+            <div class="form-group col-sm-5">
               <div class="input-group">
-                <div class="input-group-addon font-20"><?php echo $selected_value['extra_name']; ?></div>
+                <div class="input-group-addon font-20 <?php echo $tpl['edit'] ? "cus-w-70" : "cus-w-50" ?> cus-text-left"><?php echo $selected_value['extra_name']; ?></div>
                 <input type="text" class="form-control cus-extra" value="<?php echo " X ".$selected_value['extra_count']; ?>" disabled placeholder="qty">
                 <?php if (!$tpl['edit']) { ?>
                 <div class="input-group-addon btn btn-xs btn-danger btn-outline pj-remove-extra" data-qty="<?php echo "qty_".$i; ?>" data-id="<?php echo $selected_value['id']; ?>" data-index="<?php echo $index; ?>"><i class="fa fa-times fa-3x"></i></div>
--- app/web/js/pjAdminPos.js
+++ app/web/js/pjAdminPos.js
@@ -2623,12 +2623,12 @@ var jQuery_1_8_2 = jQuery_1_8_2 || jQuery.noConflict();
             return temp.qty_no == qty_no;
           });
           for (let i=0;i < filtered.length; i++) {
-            var child1 = $("<div>").addClass("input-group-addon font-20").text(filtered[i].extra_name);
+            var child1 = $("<div>").addClass("input-group-addon font-20 cus-w-50 cus-text-left").text(filtered[i].extra_name);
             var child2 = $("<input>").addClass("form-control cus-extra").attr({ type: "text", disabled: true }).val(" X "+filtered[i].extra_count);
             var icon = $("<i>").addClass("fa fa-times fa-3x");
             var child3 = $("<div>").addClass("input-group-addon btn btn-xs btn-danger btn-outline pj-remove-extra").attr("data-qty", filtered[i].qty_no).attr("data-id",filtered[i].id).attr("data-index", hidden_arr[i].extra_index).append(icon);
             var div2 = $("<div>").addClass("input-group").append(child1).append(child2).append(child3);
-            var div1 = $("<div>").addClass("form-group col-sm-4").append(div2);
+            var div1 = $("<div>").addClass("form-group col-sm-5").append(div2);
             
             // var td = $("<td>").append(div1);
             // var tr = $("<tr>").append(td);
